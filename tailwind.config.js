e0bbb90085e5f6cda034d36a327c47413e03c129
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-24 21:48:43 +0800

    final folder structure?

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   content: [],
-  content: ["./App.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./components/**/*{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
