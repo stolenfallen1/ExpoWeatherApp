0c6a9b33abc89a1c86a4f91ab4fe57461be9405a
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-24 21:54:16 +0800

    api key and components structure

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { Text, View } from "react-native";
 
 export default function App() {
+  const apiKey = "6f8effcef36b38afb38fc70bbfd51996";
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+
   return (
     <View className="flex flex-1 justify-center items-center">
       <Text className="font-bold text-2xl">Hello World</Text>
